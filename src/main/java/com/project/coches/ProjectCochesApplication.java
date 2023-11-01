69c29fa76692aea53f2744768179f43b4f87c940
Cristian Monsalve <cristian_monsalve62171@elpoli.edu.co>
2023-10-31 22:46:14 -0600

    estar project"

---
+++ src/main/java/com/project/coches/ProjectCochesApplication.java
@@ -0,0 +1,14 @@
+package com.project.coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Spring se basa mucho en las anotaciones, por lo que es importante saber que es cada una de ellas.
+@SpringBootApplication
+public class ProjectCochesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCochesApplication.class, args);
+    }
+
+}
